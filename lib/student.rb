03aa10b55e548de63c2738b135252a4677c7edf5
Manny Pissanos <mpissano@gmail.com>
2018-10-27 17:29:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,16 @@ class Student
         self.new_from_db(row)
      end
      
-     def self.
+     def self.all_students_in_grade_X(grade)
+       sql = <<-SQL
+        SELECT * FROM students
+        WHERE students.grade = ?
+        SQL
+        
+        DB[:conn].execute(sql, grade).collect do |row|
+          self.new_from_db(row)
+        end
+      end
       
     
       
